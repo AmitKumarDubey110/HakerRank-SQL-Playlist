5620dd6183692e48f0d9c50babb003e21b70d7af
Amit Kumar Dubey <95951027+Amit-Kr-Dubey@users.noreply.github.com>
2022-09-16 22:37:10 +0530

    Create Type of Triangle.sql

---
+++ Type of Triangle.sql
@@ -0,0 +1,15 @@
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+
+SELECT CASE
+WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+WHEN A = B AND B = C THEN 'Equilateral'
+WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+ELSE 'Scalene'
+END
+FROM TRIANGLES;
